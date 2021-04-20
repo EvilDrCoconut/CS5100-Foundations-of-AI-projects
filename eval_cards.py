1003bc2c4ea7173755f008f0f9b0e5713dbc8855
Robert Rega <robertdrega@gmail.com>
2021-04-20 11:01:42 -0400

    clean eval_cards

--- eval_cards.py
+++ eval_cards.py
@@ -20,9 +20,6 @@ def eval_cards(cards):
         else:
             ranks_by_card[rank] = [suit]
 
-    print(ranks_by_card)
-    print(suits_by_card)
-
     straight = []
     vset = 0
     maxrank = ''
@@ -35,7 +32,6 @@ def eval_cards(cards):
                 vset = 0
             if vset >= 5:
                 maxrank = vals[i]
-                #print("Straight flush exists")
                 return 25
 
     # Find four card
@@ -43,7 +39,6 @@ def eval_cards(cards):
     for rank in ranks_by_card.keys():
         if len(ranks_by_card[rank]) == 4:
             maxrank = rank
-            #print("Four of a kind exists")
             return 22
 
     # Find full house
@@ -58,7 +53,6 @@ def eval_cards(cards):
             hasPair = True
         
         if hasPair and hasThree:
-            #print("Full house exists")
             return 19
 
     vset = 0
@@ -71,7 +65,6 @@ def eval_cards(cards):
         
         if vset >= 5:
             maxrank = vals[i]
-            #print("Straight exists")
             return 17
 
     # Find flush
@@ -80,28 +73,22 @@ def eval_cards(cards):
         if len(suits_by_card[suit]) >= 5:
             for card in suits_by_card[suit]:
                 ranks.append(card[1:])
-            #print("Flush exists")
             return 15
 
     # Three of a kind
     for rank in ranks_by_card.keys():
         if len(ranks_by_card[rank]) == 3:
-            #print("Three of a kind exists")
             return 12
 
-    # Find pairs
+    # Pairs
     pairs = 0
     for rank in ranks_by_card.keys():
         if len(ranks_by_card[rank]) == 2:
             pairs += 1
         if pairs == 2:
-            #print("Two pair exists")
             return 8
         elif pairs == 1:
-            #print("One pair exists")
             return 4
-
-#eval_cards(['H2', 'D5', 'C2', 'D8', 'CK'])
 
 
 
