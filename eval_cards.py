d456abb9bdbc8997d69b5e8c6c46f795592a0d8c
Robert Rega <robertdrega@gmail.com>
2021-04-22 07:58:24 -0400

    update dict

--- eval_cards.py
+++ eval_cards.py
@@ -3,7 +3,7 @@ def eval_cards(cards):
     suits_by_card = {}
     ranks_by_card = {}
     vals = { 2: '2', 3: '3', 4: '4', 5 : '5',  6: '6',
-             7: '7', 8: '8', 9: '9', 10: '10', 11: 'J',
+             7: '7', 8: '8', 9: '9', 10: 'T', 11: 'J',
              12: 'Q', 13: 'K', 14: 'A'}
 
     for card in cards:
